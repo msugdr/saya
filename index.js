6633fe6a8c5606815ae0ff58ec79828fd69ad6e8
msugdr <msugdr@gmail.com>
2023-04-29 14:08:02 +0900

    form

--- index.js
+++ index.js
@@ -10,17 +10,29 @@ body {  width:800px;
         margin : auto;
 }
 main {
- width:800px;
-        background-color:pink;
+        width:600px;
+        height:600px;
+        background-color:black;
+        color:white;
         margin : auto;
+        padding : 10px;
 ]
 </style>
+`
+var htmlForm = `
+<div>
+<form method="get" action="https://chodoin-saya.azurewebsites.net/">
+<input type="text">
+<input type="submit" value="SUBMIT">
+</form>
+</div>
 `
 const server = http.createServer((request, response) => { 
   response.writeHead(200, {"Content-Type": "text/html"});        
         var msg="<html><head>" + htmlStyle + "</head>";
         msg += "<body><main><h1>SAYA</h1>";
         msg += " URI : " + request.url; 
+        msg += htmlForm;
         msg += "</main></body></html>"
         response.end(msg);});
 const port = process.env.PORT || 1337;
