7a53c23c1f80c7f8eb8add507945978879520e73
msugdr <msugdr@gmail.com>
2023-04-29 22:26:48 +0900

    doc

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ const server = http.createServer((request, response) => {
         var msg="<html>\n<head>\n<meta charset='utf8'>\n" + htmlStyle + "</head>";
         msg += "<body><main><h1>SAYA's HomePage!</h1>";
         [key,value] = request.url.split("=");
-        value0=value.replace('\l',"");
         value=decodeURI(value);
         if (value) {
                 fs.appendFile('doc.txt', value + '\n' , function(err) {});
