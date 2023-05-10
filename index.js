46d2891be1ecd8b20695f142faf2c2e74ec5c65e
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-10 18:37:44 +0900

    Update index.js readFile

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const http = require('http');
 const fs = require('fs');
 var html;
 var dummy;
-var filename;
+var filename="index.html";
 const server = http.createServer((request, response) => { 
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-        response.writeHead(200, {"Content-Type": "text/html"});        
+        response.writeHead(200, {"Content-Type": "text/html"});
+        html=fs.readFileSync(filename,'utf8');
         msg = `<html><body>I love you, ${request.method}, ${filename}</body></html>`;
         response.end(msg);
     }
