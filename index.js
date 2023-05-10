952eae1a73bf77e313a7283217443aa5f9b14534
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-10 18:14:49 +0900

    Update index.js request.method

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ var html;
 var dummy;
 var filename;
 const server = http.createServer((request, response) => { 
+    if (request.method === 'GET'){
         response.writeHead(200, {"Content-Type": "text/html"});        
-        msg = "<html><body>I love you, add var</body></html>";
-        response.end(msg);});
+        msg = `<html><body>I love you, ${request.method}r</body></html>`;
+        response.end(msg);
+    }
+});
 const port = process.env.PORT || 1337;
 server.listen(port);
 console.log("Server running at http://localhost:%d", port);
