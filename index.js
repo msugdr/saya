e523374ca906408ac1967c78868e758d2620e722
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 08:37:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 /////////////////////////////////////////////////////////////
 //  Test for Azure
 /////////////////////////////////////////////////////////////
-        //html=fs.readFileSync(filename,'utf8');
 const http = require('http');
 const fs = require('fs');
 var html;
 var dummy;
 var filename;
-var msg;
-
 const server = http.createServer((request, response) => { 
-//    value=request.url;
-//    fs.appendFile('doc.txt', value + "\n" , function(err) {});
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-        response.writeHead(200, {"Content-Type": "text/text"}); 
-        if (ext == 'html') {
-                value="  HTML:" + request.url;
-                fs.appendFile('doc.txt', value + "\n" , function(err) {});
-            filenameX = "index.html";
-            fs.readFile(filenameX,'utf8', function(err, data) {html=data});
-            html += `filename=${filename} filenameX=${filenameX}`;
-            response.end(html);
-        } else {
-                value="noHTML:" + request.url;
-                fs.appendFile('doc.txt', value + "\n" , function(err) {});
-            filenameX="doc.txt";
-            filenameX="index.html";
-            fs.readFile(filenameX,'utf8', function(err, data) {html=data});
-            html += `filename=${filename} filenameX=${filenameX}`;
-            response.end(html);
-        }
+        response.writeHead(200, {"Content-Type": "text/plain"});      
+        fs.readFile("index.html",'utf8', function(err, data) {html=data});
+        msg = `<html><body>I love you, ${request.method}, ${filename}</body></html>`;
+        response.end(msg + html);
 }
 });
 const port = process.env.PORT || 1337;
-
 server.listen(port);
-console.log("Server running at http://localhost:%d", port);
