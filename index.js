0de316b71af72b6fc782b8ef694e83feb7a9be50
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-10 18:08:24 +0900

    Update index.js console.log active

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer((request, response) => { 
         response.writeHead(200, {"Content-Type": "text/html"});        
-        msg = "<html><body>I live you</body></html>";
+        msg = "<html><body>I love you</body></html>";
         response.end(msg);});
 const port = process.env.PORT || 1337;
 server.listen(port);
-//console.log("Server running at http://localhost:%d", port);
+console.log("Server running at http://localhost:%d", port);
