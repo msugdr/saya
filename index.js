f20cb1e0d61b22115160020ff40f26383f7c4fc6
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 07:44:16 +0900

    Update index.js appendFile

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var filename;
 var msg;
 
 const server = http.createServer((request, response) => { 
+    value=request.url;
+    fs.appendFile('doc.txt', value + "\n" , function(err) {});
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
