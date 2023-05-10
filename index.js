50544895784978cf49494f402a9c92ab6f155980
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 08:39:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-        response.writeHead(200, {"Content-Type": "text/plain"});      
+        response.writeHead(200, {"Content-Type": "text/html"});      
         fs.readFile("index.html",'utf8', function(err, data) {html=data});
         msg = `<html><body>I love you, ${request.method}, ${filename}</body></html>`;
         response.end(msg + html);
