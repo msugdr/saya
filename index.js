022ac02590cc8d0ef613534212c068bd0420c512
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-10 18:10:52 +0900

    Update index.js add var

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 /////////////////////////////////////////////////////////////
 const http = require('http');
 const fs = require('fs');
+var html;
+var dummy;
+var filename;
 const server = http.createServer((request, response) => { 
         response.writeHead(200, {"Content-Type": "text/html"});        
-        msg = "<html><body>I love you</body></html>";
+        msg = "<html><body>I love you, add var</body></html>";
         response.end(msg);});
 const port = process.env.PORT || 1337;
 server.listen(port);
