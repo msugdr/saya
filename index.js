592cd9b38d5b6630cc57895736f541f93c93bdd5
msugdr <msugdr@gmail.com>
2023-05-11 10:30:02 +0900

    EDIT-0

--- index.js
+++ index.js
@@ -10,22 +10,11 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-<<<<<<< HEAD
         response.writeHead(200, {"Content-Type": "text/html"});      
         fs.readFile("index.html",'utf8', function(err, data) {html=data});
-        msg = `Edit-1.1: method = ${request.method}, filename = ${filename}`;
+        msg = `EDIT-0:${request.method}, ${filename}`;
         response.end(msg + html);
 }
-=======
-        if (ext === 'html') {
-            response.writeHead(200, {"Content-Type": "text/html"}); 
-            fn = "index.html";
-            fs.readFile(fn,'utf8', function(err, data) {html=data});
-            msg = `EDIT-3: method = ${request.method}, filename = ${filename}`;
-            response.end(msg + html);
-        }
-    }
->>>>>>> 8f1455d60364e0e3916fabe4f324ec817146ba30
 });
 const port = process.env.PORT || 1337;
 server.listen(port);
