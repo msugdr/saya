5b7f2ea16742411d4c8937ddfb61336cdf081d95
msugdr <msugdr@gmail.com>
2023-05-11 11:09:29 +0900

    EDIT-5

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const server = http.createServer((request, response) => {
             fn = filename;
             response.writeHead(200, {"Content-Type": "text/html"});      
             fs.readFile(fn,'utf8', function(err, data) {html=data});
-            msg = `EDIT-4:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
+            msg = `EDIT-5:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
             response.end(msg + html);
         } else {
         //    fn="index.html";
             response.writeHead(200, {"Content-Type": "text/html"});      
         //    fs.readFile(fn,'utf8', function(err, data) {html=data});
         //    msg = `EDIT-3b:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
-            response.end(msg + html);
+            response.end('OK');
         }
 }
 });
