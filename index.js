c38e72653948fdf8604f81fc6fbee8adf39406bf
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 09:16:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-        if (ext === 'html') {
-            response.writeHead(200, {"Content-Type": "text/html"});      
-            fs.readFile("index.html",'utf8', function(err, data) {html=data});
-            msg = `EDIT: method = ${request.method}, filename = ${filename}`;
-            response.end(msg + html);
-        }
-    }
+        response.writeHead(200, {"Content-Type": "text/html"});      
+        fs.readFile("index.html",'utf8', function(err, data) {html=data});
+        msg = `<html><body>I love you, ${request.method}, ${filename}</body></html>`;
+        response.end(msg + html);
+}
 });
 const port = process.env.PORT || 1337;
 server.listen(port);
