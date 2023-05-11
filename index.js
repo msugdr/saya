8f1455d60364e0e3916fabe4f324ec817146ba30
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 09:41:07 +0900

    Update index.js EDIT-3

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const server = http.createServer((request, response) => {
         [file,ext]=filename.split(".");
         if (ext === 'html') {
             response.writeHead(200, {"Content-Type": "text/html"}); 
-            fs.readFile("index.html",'utf8', function(err, data) {html=data});
-            msg = `EDIT-2: method = ${request.method}, filename = ${filename}`;
+            fn = "index.html";
+            fs.readFile(fn,'utf8', function(err, data) {html=data});
+            msg = `EDIT-3: method = ${request.method}, filename = ${filename}`;
             response.end(msg + html);
         }
     }
