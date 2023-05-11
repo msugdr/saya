73be8a2124e7c4a1f1626e0a2e3b7962a89bfef6
msugdr <99190980+msugdr@users.noreply.github.com>
2023-05-11 13:07:59 +0900

    Update index.js add POST

--- index.js
+++ index.js
@@ -10,24 +10,34 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
-            switch (ext) {
-                case "html":
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    html=fs.readFileSync(filename,'utf8');
-                    response.end(html);
-                    break;
-                case "jpg":
-                case "png":
-                case "gif":                
-                    response.writeHead(200, {"Content-Type": `img/${ext}`});
-                    var image = fs.readFileSync(filename, "binary");
-                    response.end(image,"binary");
-                    break;
-                default:
-                    response.writeHead(200, {"Content-Type": "text/html"});      
-                    msg = `EDIT-7.1b:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
-                    response.end(msg);
-            }
+        switch (ext) {
+            case "html":
+                response.writeHead(200, {"Content-Type": "text/html"});
+                html=fs.readFileSync(filename,'utf8');
+                response.end(html);
+                break;
+            case "jpg":
+            case "png":
+            case "gif":                
+                response.writeHead(200, {"Content-Type": `img/${ext}`});
+                var image = fs.readFileSync(filename, "binary");
+                response.end(image,"binary");
+                break;
+            default:
+                response.writeHead(200, {"Content-Type": "text/html"});      
+                msg = `EDIT-7.1b:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
+                response.end(msg);
+        }
+     } else if (request.method === 'POST'){
+        var data = '';
+    
+        //POSTデータを受けとる
+        request.on('data', function(chunk) {data += chunk})
+           .on('end', function() {
+            response.writeHead(200, {"Content-Type": "text/html"});
+             console.log(data);
+             response.end(data);
+            })
     }
 });
 const port = process.env.PORT || 1337;
