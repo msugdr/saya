41c423be723a0ce87a674cd327c43a0c91020ecf
msugdr <msugdr@gmail.com>
2023-05-11 10:32:50 +0900

    EDIT-1 , add fn

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const server = http.createServer((request, response) => {
     if (request.method === 'GET'){
         [dummy,filename]=request.url.split("/");
         [file,ext]=filename.split(".");
+        fn="index.html";
         response.writeHead(200, {"Content-Type": "text/html"});      
-        fs.readFile("index.html",'utf8', function(err, data) {html=data});
-        msg = `EDIT-0:${request.method}, ${filename}`;
+        fs.readFile(fn,'utf8', function(err, data) {html=data});
+        msg = `EDIT-1:${request.method}, ${filename}`;
         response.end(msg + html);
 }
 });
