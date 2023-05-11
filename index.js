962b3c4e12573c1499782991a63270aa3e34cf61
msugdr <msugdr@gmail.com>
2023-05-11 11:54:25 +0900

    EDIT-5.1

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ const server = http.createServer((request, response) => {
             fn="index.html";
             fn = filename;
             response.writeHead(200, {"Content-Type": "text/html"});      
-            fs.readFile(fn,'utf8', function(err, data) {html=data});
-            msg = `EDIT-4.1a:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
-            response.end(msg + html);
+            fs.readFile(fn,'utf8', function(err, data) {
+                html=data;
+                msg = `EDIT-5.1a:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
+                response.end(msg + html);
+            
+            });
+        // msg = `EDIT-4.1a:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
+        //  response.end(msg + html);
         } else {
             fn="index.html";
             response.writeHead(200, {"Content-Type": "text/html"});      
-        //    fs.readFile(fn,'utf8', function(err, data) {html=data});
-            msg = `EDIT-4.1b:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
+            fs.readFile(fn,'utf8', function(err, data) {html=data});
+            msg = `EDIT-5.1b:method = ${request.method}, filename = ${filename}, ext = ${ext}`;
             response.end(msg);
         }
 }
@@ -30,3 +35,4 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 // EDIT-4.1  response.end(msg + html); -> response.end(msg);
 // EDIT-5  del fs.readFile(fn,'utf8', function(err, data) {html=data});
+// EDIT-5.1
